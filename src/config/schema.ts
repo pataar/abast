830a971de83b09696ff2d3ac50530f0d58bef7a0
pataar <git@pataar.nl>
2023-10-18 01:00:17 +0200

    better config parsing

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -3,18 +3,20 @@ import { z } from "zod";
 
 const zs = (shape: ZodRawShape) => z.object(shape).strict().optional();
 
-export const configSchema = zs({
-	openai: zs({
-		key: z.string(),
-	}),
-	gitlab: zs({
-		host: z.string().default("https://gitlab.com"),
-		token: z.string(),
-	}),
-	issue: zs({
-		template: z.string().default(""),
-	}),
-});
+export const configSchema = z
+	.object({
+		openai: zs({
+			key: z.string(),
+		}),
+		gitlab: zs({
+			host: z.string().default("https://gitlab.com"),
+			token: z.string(),
+		}),
+		issue: zs({
+			template: z.string().default(""),
+		}),
+	})
+	.strict();
 
 export type ConfigValues = z.infer<typeof configSchema>;
 export const defaults = configSchema.parse({});
